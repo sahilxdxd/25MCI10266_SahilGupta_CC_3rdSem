394642a5dd4397dc88bef2bbbbdd8317f1a15432
Sahil Gupta <guptasahil6072@gmail.com>
2026-09-21 16:09:24 +0530

    Add DFS for minimum road weight calculation
    Implement DFS to find minimum road weight in a graph.

--- experiment_3/leetcode_2592_.cpp
+++ experiment_3/leetcode_2592_.cpp
@@ -1,1 +1,44 @@
+class Solution {
+public:
+    void dfs(int node, vector<vector<pair<int,int>>>& adj,
+             vector<int>& visited, int& ans) {
 
+        visited[node] = 1;
+
+        for (auto it : adj[node]) {
+
+            int next = it.first;
+            int weight = it.second;
+
+            ans = min(ans, weight);
+
+            if (!visited[next]) {
+                dfs(next, adj, visited, ans);
+            }
+        }
+    }
+
+    int minScore(int n, vector<vector<int>>& roads) {
+
+        vector<vector<pair<int,int>>> adj(n + 1);
+
+        // Build graph
+        for (auto road : roads) {
+
+            int u = road[0];
+            int v = road[1];
+            int weight = road[2];
+
+            adj[u].push_back({v, weight});
+            adj[v].push_back({u, weight});
+        }
+
+        vector<int> visited(n + 1, 0);
+
+        int ans = INT_MAX;
+
+        dfs(1, adj, visited, ans);
+
+        return ans;
+    }
+};
